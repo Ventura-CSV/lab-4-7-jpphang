bc9bb4b67acd0cb11d25fb7c98e3a9959caf2615
jpphang <joyanna_phang1@my.vcccd.edu>
2024-06-29 04:17:06 -0700

    47

--- main.py
+++ main.py
@@ -3,20 +3,17 @@ def main():
     pre_val = None
     
     while True:
+        cur_val = int(input("Enter a number here: "))
         
-        cur_val = int(input("Enter a number here:"))
-        
-        if pre_val is not None and cur_val <= pre_val:
+        if pre_val is None or cur_val <= pre_val:
             numbers.append(cur_val)
-            
         else:
-            if pre_val is not None:
-                break
+            break
+
         pre_val = cur_val
 
-    print(*numbers)
+    print("Collected numbers:", *numbers)
     return numbers
-
 
 if __name__ == '__main__':
     main()
